830772ae4e6194c649b700ea36f311979940c615
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-08-31 19:07:15 -0300

    feat: ensure FacebookAuthentication usecase calls LoadFacebookUserApi only once

--- tests/data/usecases/facebook-authentication.spec.ts
+++ tests/data/usecases/facebook-authentication.spec.ts
@@ -4,28 +4,46 @@ import { FacebookAuthenticationUsecase } from "@/data/usecases";
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
+  callsCount = 0;
   result? = undefined;
 
   async loadUser(
     params: LoadFacebookUserApi.Params
   ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token;
+    this.callsCount++;
     return this.result;
   }
 }
 
+type SutTypes = {
+  sut: FacebookAuthenticationUsecase;
+  loadFacebookUserApiSpy: LoadFacebookUserApiSpy;
+};
+
+const makeSut = (
+  loadFacebookUserApiSpy = new LoadFacebookUserApiSpy()
+): SutTypes => {
+  const sut = new FacebookAuthenticationUsecase(loadFacebookUserApiSpy);
+
+  return {
+    sut,
+    loadFacebookUserApiSpy,
+  };
+};
+
 describe("FacebookAuthentication Usecase", () => {
   test("Should call LoadFacebookUserApi with correct params", async () => {
-    const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy();
-    const sut = new FacebookAuthenticationUsecase(loadFacebookUserApiSpy);
+    const { sut, loadFacebookUserApiSpy } = makeSut();
     await sut.perform({ token: "any_token" });
     expect(loadFacebookUserApiSpy.token).toBe("any_token");
+    expect(loadFacebookUserApiSpy.callsCount).toBe(1);
   });
 
   test("Should return AuthenticationError if LoadFacebookUserApi returns undefined", async () => {
     const loadFacebookUserApiSpy = new LoadFacebookUserApiSpy();
     loadFacebookUserApiSpy.result = undefined;
-    const sut = new FacebookAuthenticationUsecase(loadFacebookUserApiSpy);
+    const { sut } = makeSut(loadFacebookUserApiSpy);
     const authResult = await sut.perform({ token: "any_token" });
     expect(authResult).toEqual(new AuthenticationError());
   });
