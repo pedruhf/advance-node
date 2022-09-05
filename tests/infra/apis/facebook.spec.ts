2244c978d33bed0fb92f03937e4a539781d192f0
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-05 14:06:22 -0300

    feat: ensure FacebookApi gets debug token

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,7 +1,6 @@
 import { FacebookApi } from "@/infra/apis";
 import { HttpGetClientSpy } from "@/tests/infra/mocks";
 
-
 type SutTypes = {
   sut: FacebookApi;
   htpGetClientSpy: HttpGetClientSpy;
@@ -21,18 +20,34 @@ const makeSut = (): SutTypes => {
 };
 
 describe("FacebookApi", () => {
-  test("Should calls HttpGetClient with correct params", async () => {
+  test("Should get app token", async () => {
     const { sut, htpGetClientSpy } = makeSut();
+    const getSpy = jest.spyOn(htpGetClientSpy, "get");
 
     await sut.loadUser({ token: "any_client_token" });
 
-    expect(htpGetClientSpy.url).toBe(
-      "https://graph.facebook.com/oauth/access_token"
-    );
-    expect(htpGetClientSpy.params).toEqual({
-      client_id: clientId,
-      client_secret: clientSecret,
-      grant_type: "client_credentials",
+    expect(getSpy).toHaveBeenCalledWith({
+      url: "https://graph.facebook.com/oauth/access_token",
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: "client_credentials",
+      },
+    });
+  });
+
+  test("Should get debug token", async () => {
+    const { sut, htpGetClientSpy } = makeSut();
+    const getSpy = jest.spyOn(htpGetClientSpy, "get");
+
+    await sut.loadUser({ token: "any_client_token" });
+
+    expect(getSpy).toHaveBeenCalledWith({
+      url: "https://graph.facebook.com/debug_token",
+      params: {
+        access_token: "any_app_token",
+        input_token: "any_client_token",
+      },
     });
   });
 });
--- tests/infra/mocks/facebook.ts
+++ tests/infra/mocks/facebook.ts
@@ -3,9 +3,13 @@ import { HttpGetClient } from "@/infra/http";
 export class HttpGetClientSpy implements HttpGetClient {
   url?: string;
   params?: any;
+  result: any = {
+    access_token: "any_app_token",
+  };
 
-  async get(params: HttpGetClient.Params): Promise<void> {
+  async get(params: HttpGetClient.Params): Promise<HttpGetClient.Result> {
     this.url = params.url;
     this.params = params.params;
+    return this.result;
   }
 }
