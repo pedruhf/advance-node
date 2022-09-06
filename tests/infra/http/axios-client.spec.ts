b586343169aa6ad2a529e7e86b85afe518d05e11
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-05 21:48:36 -0300

    refactor: move sut creation to a factory method

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -9,22 +9,34 @@ class AxiosHttpClient {
   }
 }
 
-describe("AxiosHttpClient", () => {
-  test("Shoul call get with correct params", async () => {
-    const fakeAxios = axios as jest.Mocked<typeof axios>;
-    const sut = new AxiosHttpClient();
+const makeSut = () => {
+  const sut = new AxiosHttpClient();
 
+  return { sut };
+};
+
+describe("AxiosHttpClient", () => {
+  let fakeAxios: jest.Mocked<typeof axios>;
+  let url: string;
+  let params: object;
+
+  beforeAll(() => {
+    fakeAxios = axios as jest.Mocked<typeof axios>;
+    url = "any_url";
+    params = {
+      anyField: "any_value",
+    };
+  });
+
+  test("Shoul call get with correct params", async () => {
+    const { sut } = makeSut();
     await sut.get({
-      url: "any_url",
-      params: {
-        anyField: "any_value",
-      },
+      url,
+      params,
     });
 
-    expect(fakeAxios.get).toHaveBeenCalledWith("any_url", {
-      params: {
-        anyField: "any_value",
-      },
+    expect(fakeAxios.get).toHaveBeenCalledWith(url, {
+      params,
     });
     expect(fakeAxios.get).toHaveBeenCalledTimes(1);
   });
