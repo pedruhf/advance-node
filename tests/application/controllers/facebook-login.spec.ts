528cf8ef932d472f1fd6d7405b1bf3e2eff83a77
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-08 09:34:36 -0300

    feat: enure FacebookLoginController calls FacebookAuthentication with correct params

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,7 +1,16 @@
+import { FacebookAuthenticationUsecase } from "@/data/usecases";
+import { FacebookAuthentication } from "@/domain/features";
+import { AccessToken } from "@/domain/models";
+
 type HttpResponse = { statusCode: number; data: any };
 
 class FacebookLoginController {
+  constructor(
+    private readonly facebookAuthentication: FacebookAuthentication
+  ) {}
+
   async handle(httpRequest: any): Promise<HttpResponse> {
+    await this.facebookAuthentication.perform({ token: httpRequest.token });
     return {
       statusCode: 400,
       data: new Error("The field token is required"),
@@ -9,9 +18,23 @@ class FacebookLoginController {
   }
 }
 
+class FacebookAuthenticationSpy implements FacebookAuthentication {
+  callsCount = 0;
+  data?: FacebookAuthentication.Params;
+
+  async perform(
+    params: FacebookAuthentication.Params
+  ): Promise<FacebookAuthentication.Result> {
+    this.callsCount++;
+    this.data = params;
+    return new AccessToken("any_token");
+  }
+}
+
 describe("FacebookLoginController", () => {
   test("Should return 400 if token is empty", async () => {
-    const sut = new FacebookLoginController();
+    const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+    const sut = new FacebookLoginController(facebookAuthenticationSpy);
     const httpResponse = await sut.handle({ token: "" });
 
     expect(httpResponse).toEqual({
@@ -21,7 +44,8 @@ describe("FacebookLoginController", () => {
   });
 
   test("Should return 400 if token is null", async () => {
-    const sut = new FacebookLoginController();
+    const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+    const sut = new FacebookLoginController(facebookAuthenticationSpy);
     const httpResponse = await sut.handle({ token: null });
 
     expect(httpResponse).toEqual({
@@ -30,13 +54,23 @@ describe("FacebookLoginController", () => {
     });
   });
 
-  test("Should return 400 if token is null", async () => {
-    const sut = new FacebookLoginController();
+  test("Should return 400 if token is undefined", async () => {
+    const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+    const sut = new FacebookLoginController(facebookAuthenticationSpy);
     const httpResponse = await sut.handle({ token: undefined });
 
     expect(httpResponse).toEqual({
       statusCode: 400,
       data: new Error("The field token is required"),
     });
+  });
+
+  test("Should call FacebookAuthentication with correct params", async () => {
+    const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+    const sut = new FacebookLoginController(facebookAuthenticationSpy);
+    await sut.handle({ token: "any_token" });
+
+    expect(facebookAuthenticationSpy.data).toEqual({ token: "any_token" });
+    expect(facebookAuthenticationSpy.callsCount).toBe(1);
   });
 });
