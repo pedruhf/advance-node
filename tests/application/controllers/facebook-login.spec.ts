867dde9d282c3777a011e4c37391550d06281839
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-08 09:51:35 -0300

    feat: enure FacebookLoginController returns 401 if authentication fails

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,5 +1,5 @@
+import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
-import { AccessToken } from "@/domain/models";
 
 type HttpResponse = { statusCode: number; data: any };
 
@@ -9,10 +9,16 @@ class FacebookLoginController {
   ) {}
 
   async handle(httpRequest: any): Promise<HttpResponse> {
-    await this.facebookAuthentication.perform({ token: httpRequest.token });
+    if (!httpRequest.token) {
+      return {
+        statusCode: 400,
+        data: new Error("The field token is required"),
+      };
+    }
+    const result = await this.facebookAuthentication.perform({ token: httpRequest.token });
     return {
-      statusCode: 400,
-      data: new Error("The field token is required"),
+      statusCode: 401,
+      data: result,
     };
   }
 }
@@ -20,13 +26,14 @@ class FacebookLoginController {
 class FacebookAuthenticationSpy implements FacebookAuthentication {
   callsCount = 0;
   data?: FacebookAuthentication.Params;
+  result = new AuthenticationError();
 
   async perform(
     params: FacebookAuthentication.Params
   ): Promise<FacebookAuthentication.Result> {
     this.callsCount++;
     this.data = params;
-    return new AccessToken("any_token");
+    return this.result;
   }
 }
 
@@ -35,8 +42,9 @@ type SutTypes = {
   facebookAuthenticationSpy: FacebookAuthenticationSpy;
 };
 
-const makeSut = (): SutTypes => {
-  const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+const makeSut = (
+  facebookAuthenticationSpy = new FacebookAuthenticationSpy()
+): SutTypes => {
   const sut = new FacebookLoginController(facebookAuthenticationSpy);
 
   return {
@@ -82,5 +90,17 @@ describe("FacebookLoginController", () => {
 
     expect(facebookAuthenticationSpy.data).toEqual({ token: "any_token" });
     expect(facebookAuthenticationSpy.callsCount).toBe(1);
+  });
+
+  test("Should return 401 if authentication fails", async () => {
+    const facebookAuthenticationSpy = new FacebookAuthenticationSpy();
+    facebookAuthenticationSpy.result = new AuthenticationError();
+    const { sut } = makeSut();
+    const httpResponse = await sut.handle({ token: "any_token" });
+
+    expect(httpResponse).toEqual({
+      statusCode: 401,
+      data: new AuthenticationError(),
+    });
   });
 });
