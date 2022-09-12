b6393eda51a176866207e4203a2ef9ce2a48446f
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-12 13:59:03 -0300

    refactor: move sut creation to a factory method

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -2,20 +2,34 @@ import { FacebookApi } from "@/infra/apis";
 import { AxiosHttpClient } from "@/infra/http";
 import { env } from "@/main/config/env";
 
+type SutTypes = {
+  sut: FacebookApi;
+  axiosClient: AxiosHttpClient;
+};
+
+const makeSut = (): SutTypes => {
+  const axiosClient = new AxiosHttpClient();
+  const sut = new FacebookApi(
+    axiosClient,
+    env.facebookApi.clientId,
+    env.facebookApi.clientSecret
+  );
+
+  return {
+    sut,
+    axiosClient,
+  };
+};
+
 describe("Facebook API Integration Test", () => {
   test("Should return a Facebook User if token is valid", async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    );
-
+    const { sut } = makeSut();
     const fbUser = await sut.loadUser({
       token:
         // eslint-disable-next-line max-len
         "EAAGzyzBSZAZBsBAAZCY925qjqTmFbSacwO63BgZAM4HkmMvL7gD3PFJ6Xv7v3c5ghggq7LlRLPKJ6FoKWVHSoRFIhj1fY2x5hJgmNvkJvpT2OqqPeMKUo8l3hRbT7ZCel6sa6KT3k0YLDnhNkyiQLaOMD95aYWZC03LYmHbdfuIXbBFOSZB1q9KGO1p7rB7RVYj4DZAovsDRnAZDZD",
     });
+
     expect(fbUser).toEqual({
       facebookId: "109792411875504",
       email: "pedro_kchbkry_teste@tfbnw.net",
@@ -24,13 +38,7 @@ describe("Facebook API Integration Test", () => {
   });
 
   test("Should return undefined if token is invalid", async () => {
-    const axiosClient = new AxiosHttpClient();
-    const sut = new FacebookApi(
-      axiosClient,
-      env.facebookApi.clientId,
-      env.facebookApi.clientSecret
-    );
-
+    const { sut } = makeSut();
     const fbUser = await sut.loadUser({
       token: "invalid_token",
     });
