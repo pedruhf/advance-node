7c3dd8a8b538252ebaacdb91ce284620b19289c3
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-13 20:42:57 -0300

    feat: ensure ExpressRouter calls handle with empty request

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -1,26 +1,51 @@
 import { Request, Response } from "express";
-import { getMockReq, getMockRes} from "@jest-mock/express";
+import { getMockReq, getMockRes } from "@jest-mock/express";
 
 import { Controller } from "@/application/controllers";
 import { ControllerStub } from "@/tests/application/mocks";
 
 class ExpressRouter {
-  constructor (private readonly controller: Controller) {}
-  async adapt (req: Request, res: Response): Promise<void> {
+  constructor(private readonly controller: Controller) {}
+  async adapt(req: Request, res: Response): Promise<void> {
     await this.controller.perform({ ...req.body });
   }
 }
+
+type SutTypes = {
+  sut: ExpressRouter;
+  controllerSpy: ControllerStub
+}
+
+const makeSut = (): SutTypes => {
+  const controllerSpy = new ControllerStub();
+  const sut = new ExpressRouter(controllerSpy);
+
+  return {
+    sut,
+    controllerSpy,
+  };
+};
 
 describe("ExpressRouter", () => {
   test("Should call handle with correct request", async () => {
     const req = getMockReq({ body: { anyField: "any_value" } });
     const { res } = getMockRes();
 
-    const controllerStub = new ControllerStub();
-    const performSpy = jest.spyOn(controllerStub, "perform");
-    const sut = new ExpressRouter(controllerStub);
+    const { sut, controllerSpy } = makeSut();
+    const performSpy = jest.spyOn(controllerSpy, "perform");
     await sut.adapt(req, res);
 
     expect(performSpy).toHaveBeenLastCalledWith({ anyField: "any_value" });
+  });
+
+  test("Should call handle with empty request", async () => {
+    const req = getMockReq();
+    const { res } = getMockRes();
+
+    const { sut, controllerSpy } = makeSut();
+    const performSpy = jest.spyOn(controllerSpy, "perform");
+    await sut.adapt(req, res);
+
+    expect(performSpy).toHaveBeenLastCalledWith({});
   });
 });
