85864a060f4e7ef73dbe689523ce99390ef0dff9
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-19 16:37:37 -0300

    feat: ensure ChangeProfilePicture calls uploadFile with unique key

--- tests/data/use-cases/change-profile-picture.spec.ts
+++ tests/data/use-cases/change-profile-picture.spec.ts
@@ -1,31 +1,44 @@
 import { mock } from "jest-mock-extended";
 
-type Setup = (fileStorage: UploadFile) => ChangeProfilePicture;
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture;
 type Input = { userId: string; file: Buffer };
 type ChangeProfilePicture = (input: Input) => Promise<void>;
 
-const setupChangeProfilePicture: Setup = (fileStorage) => {
+const setupChangeProfilePicture: Setup = (fileStorage, crypto) => {
   return async ({ userId, file }) => {
-    await fileStorage.upload({ file, key: userId });
+    const uuid = crypto.uuid({ key: userId });
+    await fileStorage.upload({ file, key: uuid });
   };
 };
 
 interface UploadFile {
-  upload: (inpput: UploadFile.Input) => Promise<void>;
+  upload: (input: UploadFile.Input) => Promise<void>;
 }
 
 namespace UploadFile {
   export type Input = { file: Buffer; key: string };
 }
 
+interface UUIDGenerator {
+  uuid: (input: UUIDGenerator.Input) => UUIDGenerator.Output;
+}
+
+namespace UUIDGenerator {
+  export type Input = { key: string };
+  export type Output = string;
+}
+
 describe("ChangeProfilePicture UseCase", () => {
   test("Should call UploadFile with correct input", async () => {
+    const uuid = "any_unique_id";
     const file = Buffer.from("any_buffer");
     const fileStorage = mock<UploadFile>();
-    const sut = setupChangeProfilePicture(fileStorage);
+    const crypto = mock<UUIDGenerator>();
+    crypto.uuid.mockReturnValue(uuid);
+    const sut = setupChangeProfilePicture(fileStorage, crypto);
     await sut({ userId: "any_user_id", file });
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: "any_user_id" });
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid });
     expect(fileStorage.upload).toHaveBeenCalledTimes(1);
   });
 });
