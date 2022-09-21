e4fbae74d0461693bea52f2d6535657b838edc10
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-21 13:21:49 -0300

    refactor: isolate sut creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -12,8 +12,13 @@ class UUIDHandler implements UUIDGenerator {
 }
 
 describe("UUID Handler", () => {
+  let sut: UUIDHandler;
+
+  beforeEach(() => {
+    sut = new UUIDHandler();
+  });
+
   test("Should call uuid.v4", () => {
-    const sut = new UUIDHandler();
     sut.uuid({ key: "any_key " });
 
     expect(v4).toHaveBeenCalledTimes(1);
@@ -22,7 +27,6 @@ describe("UUID Handler", () => {
   test("Should return correct value", () => {
     jest.mocked(v4).mockReturnValueOnce("any_uuid");
 
-    const sut = new UUIDHandler();
     const uuid = sut.uuid({ key: "any_key" });
 
     expect(uuid).toBe("any_key_any_uuid");
