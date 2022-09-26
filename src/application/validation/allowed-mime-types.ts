e23097edbbf563829b691a69791f6cee1eab3fcc
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-26 14:48:35 -0300

    refactor: move AllowedMimeTypesValidator to its own file

---
+++ src/application/validation/allowed-mime-types.ts
@@ -0,0 +1,21 @@
+import { InvalidMimeTypeError } from "@/application/errors";
+
+type Extension = "png" | "jpg";
+
+export class AllowedMimeTypesValidator {
+  constructor(private readonly allowed: Extension[], private readonly mimeType: string) {}
+
+  validate(): Error | undefined {
+    if (this.isPng()) return;
+    if (this.isJpg()) return;
+    return new InvalidMimeTypeError(this.allowed);
+  }
+
+  private isPng(): boolean {
+    return this.allowed.includes("png") && this.mimeType === "image/png";
+  }
+
+  private isJpg(): boolean {
+    return this.allowed.includes("jpg") && /image\/jpe?g/.test(this.mimeType);
+  }
+}
