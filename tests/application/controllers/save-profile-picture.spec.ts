5dcb310b8a83e92be5ed4d0153e82c736fbd6ec0
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-26 09:31:32 -0300

    feat: ensure SaveProfilePicture Controller returns 400 if file is empty

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -1,7 +1,7 @@
 import { RequiredFieldError } from "@/application/errors";
 import { badRequest, HttpResponse, HttpStatusCode } from "@/application/helpers";
 
-type HttpRequest = { file: any };
+type HttpRequest = { file: { buffer: Buffer } };
 type Model = Error;
 
 class SaveProfilePicture {
@@ -18,7 +18,7 @@ describe("SaveProfilePicture Controller", () => {
   });
 
   test("Should return 400 if file is not provided", async () => {
-    const httpResponse = await sut.handle({ file: undefined });
+    const httpResponse = await sut.handle({ file: undefined as any });
 
     expect(httpResponse).toEqual({
       statusCode: HttpStatusCode.badRequest,
@@ -27,7 +27,16 @@ describe("SaveProfilePicture Controller", () => {
   });
 
   test("Should return 400 if file is not provided", async () => {
-    const httpResponse = await sut.handle({ file: null });
+    const httpResponse = await sut.handle({ file: null as any });
+
+    expect(httpResponse).toEqual({
+      statusCode: HttpStatusCode.badRequest,
+      data: new RequiredFieldError("foto"),
+    });
+  });
+
+  test("Should return 400 if file is empty", async () => {
+    const httpResponse = await sut.handle({ file: { buffer: Buffer.from("") } });
 
     expect(httpResponse).toEqual({
       statusCode: HttpStatusCode.badRequest,
