5e50d341b432301098944d7344479eddda3fcfbd
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-28 10:13:13 -0300

    refactor: move typeorm connection options from .env

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,14 +1,11 @@
 import "./config/module-alias";
 import "reflect-metadata";
-import { createConnection, getConnectionOptions } from "typeorm";
+import { createConnection } from "typeorm";
 
 import { env } from "@/main/config/env";
 
-getConnectionOptions()
-  .then(async (options) => {
-    const root = process.env.TS_NODE_DEV ? "src" : "dist/src";
-    const entities = [`${root}/infra/repos/postgres/entities/index.{ts,js}`];
-    await createConnection({ ...options, entities });
+createConnection()
+  .then(async () => {
     const { app } = await import("@/main/config/app");
     app.listen(env.port, () => console.log(`Server is running at http://localhost:${env.port}`));
   })
