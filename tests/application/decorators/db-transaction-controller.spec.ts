83d31b84feeb1b51fd3c9a72587297cbaf596bd9
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-28 19:45:42 -0300

    refactor: move sut creation to a factory method

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -1,7 +1,7 @@
 import { HttpResponse } from "@/application/helpers";
 
 class DbTransactionControllerDecorator {
-  constructor (private readonly db: DbTransaction) {}
+  constructor(private readonly db: DbTransaction) {}
 
   async perform(httpRequest: any): Promise<void> {
     await this.db.openTransaction();
@@ -13,14 +13,28 @@ interface DbTransaction {
 }
 
 class DbTransactionSpy implements DbTransaction {
-  async openTransaction (): Promise<void> {}
+  async openTransaction(): Promise<void> {}
 }
+
+type SutTypes = {
+  sut: DbTransactionControllerDecorator;
+  dbTransactionSpy: DbTransactionSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const dbTransactionSpy = new DbTransactionSpy();
+  const sut = new DbTransactionControllerDecorator(dbTransactionSpy);
+
+  return {
+    sut,
+    dbTransactionSpy,
+  };
+};
 
 describe("DbTransactionControllerDecorator", () => {
   test("Should open transaction", async () => {
-    const dbTransactionSpy = new DbTransactionSpy();
+    const { sut, dbTransactionSpy } = makeSut();
     const openTransactionSpy = jest.spyOn(dbTransactionSpy, "openTransaction");
-    const sut = new DbTransactionControllerDecorator(dbTransactionSpy);
     await sut.perform({ any: "any" });
 
     expect(openTransactionSpy).toHaveBeenCalled();
