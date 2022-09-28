819a1ba6dad87a67fd2faa8cab2c86fb40d58688
Pedro Freitas <pedroh.ufrn@gmail.com>
2022-09-28 20:00:09 -0300

    feat: ensure DbTransactionControllerDecorator calls rollback and close transactions on failure

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -9,9 +9,14 @@ class DbTransactionControllerDecorator {
 
   async perform(httpRequest: any): Promise<void> {
     await this.db.openTransaction();
-    await this.decoratee.perform(httpRequest);
-    await this.db.commitTransaction();
-    await this.db.closeTransaction();
+    try {
+      await this.decoratee.perform(httpRequest);
+      await this.db.commitTransaction();
+      await this.db.closeTransaction();
+    } catch {
+      await this.db.rollbackTransaction();
+      await this.db.closeTransaction();
+    }
   }
 }
 
@@ -19,12 +24,14 @@ interface DbTransaction {
   openTransaction: () => Promise<void>;
   closeTransaction: () => Promise<void>;
   commitTransaction: () => Promise<void>;
+  rollbackTransaction: () => Promise<void>;
 }
 
 class DbTransactionSpy implements DbTransaction {
   async openTransaction(): Promise<void> {}
   async closeTransaction(): Promise<void> {}
   async commitTransaction(): Promise<void> {}
+  async rollbackTransaction(): Promise<void> {}
 }
 
 type SutTypes = {
@@ -67,11 +74,28 @@ describe("DbTransactionControllerDecorator", () => {
   test("Should call commit and close transactions on success", async () => {
     const { sut, dbTransactionSpy } = makeSut();
     const commitTransactionSpy = jest.spyOn(dbTransactionSpy, "commitTransaction");
+    const rollbackTransactionSpy = jest.spyOn(dbTransactionSpy, "rollbackTransaction");
     const closeTransactionSpy = jest.spyOn(dbTransactionSpy, "closeTransaction");
     await sut.perform({ any: "any" });
 
     expect(commitTransactionSpy).toHaveBeenCalled();
     expect(commitTransactionSpy).toHaveBeenCalledTimes(1);
+    expect(rollbackTransactionSpy).not.toHaveBeenCalled();
+    expect(closeTransactionSpy).toHaveBeenCalled();
+    expect(closeTransactionSpy).toHaveBeenCalledTimes(1);
+  });
+
+  test("Should call commit and close transactions on success", async () => {
+    const { sut, dbTransactionSpy, decorateeSpy } = makeSut();
+    jest.spyOn(decorateeSpy, "perform").mockRejectedValueOnce(new Error("decoratee_error"));
+    const commitTransactionSpy = jest.spyOn(dbTransactionSpy, "commitTransaction");
+    const rollbackTransactionSpy = jest.spyOn(dbTransactionSpy, "rollbackTransaction");
+    const closeTransactionSpy = jest.spyOn(dbTransactionSpy, "closeTransaction");
+    await sut.perform({ any: "any" });
+
+    expect(commitTransactionSpy).not.toHaveBeenCalled();
+    expect(rollbackTransactionSpy).toHaveBeenCalled();
+    expect(rollbackTransactionSpy).toHaveBeenCalledTimes(1);
     expect(closeTransactionSpy).toHaveBeenCalled();
     expect(closeTransactionSpy).toHaveBeenCalledTimes(1);
   });
